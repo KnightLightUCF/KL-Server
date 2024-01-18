2248fab4257d72d163a2d33930d1c5baf13e1615
MiniJ147 <jakepaul628@gmail.com>
2024-01-18 15:14:19 -0500

    Added Windows start console prompt

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import os
 
-msg = "Welcome to the start of the server wrapper"
+'''
+holds the start logic for windows. Each OS will be different. 
+'''
+def start_cmd_windows():
+    '''
+    starts up console. /k makes the program stay online. /c would close it.
+    TODO: We need to make the console prompt turn on with a python ide and then run poetry commands.
+    TODO: Make the cmd start as a subprocess under the OS so we can track it. 
+
+    os.system("start cmd /k {commands})
+    '''
+
+    os.system("start cmd /k cd..")
+
+'''
+Will hold the runtime of our program
+'''
+def run():
+    pass
+        
 
 if __name__ == "__main__":
-    print(msg)
+    start_cmd_windows()
+
+    run()
