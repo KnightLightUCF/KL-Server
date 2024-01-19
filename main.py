e66ecf0d7ef8ed4904090132b2650fcc46f1f11a
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-01-18 22:12:52 -0500

    Added Process killing [NOT FINAL]

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import subprocess
 
+DEFAULT_IT_COUNT = 10000000 #testing can edit 
+
+pid = None #used to track the spinned up process 
+
 '''
 holds the start logic for windows. Each OS will be different. 
 '''
 def start_cmd_windows():
     '''
-    starts up console. /k makes the program stay online. /c would close it.
-    TODO: URGENT figure out a way to kill program after it is spinned up.
     TODO: We need to make the console prompt turn on with a python ide and then run poetry commands.
 
     subprocess.Popen(['command1','command2'], new process arguments)
@@ -14,21 +16,41 @@ def start_cmd_windows():
     DOCS: https://docs.python.org/3/library/subprocess.html
     '''
 
-    pro = subprocess.Popen(['start', 'cmd', '/k', 'windows\\startup.bat'], shell=True)
+    global pid
+    
+    si = subprocess.STARTUPINFO()
+    si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    pro = subprocess.Popen(['cmd', '/c', 'cd','windows','&','startup.bat'], startupinfo=si)
+    pid = pro.pid
+
+    print("Processes ID: "+str(pid))
     
 
 def shutdown_cmd_windows():
-    pass
+    command = ['TASKKILL', '/F', '/T', '/PID', str(pid)]
+    subprocess.Popen(command)
+    print("killed")
 
 '''
 Will hold the runtime of our program
+
+For now it_count will be to test how long until we kill the subprocess
 '''
-def run():
+def run(it_count):
+    i=0
+    #test code to show that both processes run at the sametime
+    while True: 
+        if i>=it_count:
+            shutdown_cmd_windows()
+            break
+
+        i+=1
+
     input("Press any key to exit... \n>>> ")
         
 
 if __name__ == "__main__":
     start_cmd_windows()
 
-    run()
+    run(it_count=DEFAULT_IT_COUNT)
     
